cd4825e3d7760bf5e898197f71ce9fc7837cb6fa
DariaSavu <143889190+DariaSavu@users.noreply.github.com>
2024-10-11 08:23:31 +0300

    +1

--- src/Components/GeneratedImages/GeneratedImages.tsx
+++ src/Components/GeneratedImages/GeneratedImages.tsx
@@ -1,10 +1,7 @@
-import { Context } from "vm";
 import { GeneratedImagesProps } from "./GeneratedImagesProps";
 import { generatedImagesClassName } from "./GeneratedImagesStyles";
 
 const GeneratedImages = (props: GeneratedImagesProps): JSX.Element => {
-    const images=require.context('public\Images',true);
-
     return (
         <div className={generatedImagesClassName}>
             <h3>Generated Images</h3>
